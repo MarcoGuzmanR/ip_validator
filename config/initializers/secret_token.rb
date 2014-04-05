d2a163b931c39403eb4fd8ef3407e3a2855f7bc0
Marco Guzmán <marco.guzman@tangosource.com>
2014-04-05 09:14:44 -0600

    Added rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IpValidator::Application.config.secret_key_base = '9f4bfc2fd3451bffebdaf2976c4121690bacb3d58000fe40ae1afb2a77d7604e4d5433d56e8be8ddf5137af811c6d479a87c17dce419993a8f54bb57b3bb81cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IpValidator::Application.config.session_store :cookie_store, key: '_ip_validator_session'
